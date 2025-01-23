04c215c703cdd03d56877835b515a44d0de980b2
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-01-22 18:56:22 -0800

    Add project files.

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,37 @@
+﻿//*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Discount Calculator//Assignment2
+
+namespace COMP003A.Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            int userAge;
+            double productPrice;
+            bool isStudent;
+
+            Console.Write("What is your first name: ");
+            userName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            userName += Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            userAge = int.Parse(Console.ReadLine());
+
+            Console.Write("What is the price of the item you're interest in? ");
+            productPrice = double.Parse(Console.ReadLine());
+
+            Console.Write("Are you a student? (true or false): ");
+            isStudent = bool.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
