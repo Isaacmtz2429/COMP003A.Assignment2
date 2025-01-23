741d36f2a7f584b798fc9a7fb5132f2e2951b729
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-01-22 20:16:58 -0800

    Assignment 2 Updated

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -10,16 +10,21 @@ namespace COMP003A.Assignment2
     {
         static void Main(string[] args)
         {
+            // Information
             string userName;
+            string lastName;
             int userAge;
             double productPrice;
             bool isStudent;
 
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("Welcome to the Discount Calculator");
+            Console.WriteLine("**************************************************************");
             Console.Write("What is your first name: ");
             userName = Console.ReadLine();
 
             Console.Write("What is your last name: ");
-            userName += Console.ReadLine();
+            lastName = Console.ReadLine();
 
             Console.Write("How old are you? ");
             userAge = int.Parse(Console.ReadLine());
@@ -30,8 +35,21 @@ namespace COMP003A.Assignment2
             Console.Write("Are you a student? (true or false): ");
             isStudent = bool.Parse(Console.ReadLine());
 
+            // Calculations
+            int futureAge = userAge + 5;
+            double StudentDiscount = productPrice * .9;
+            double SeniorCitizen = productPrice * .8;
 
 
+
+            // Outputs
+            Console.WriteLine($"\n**************************************************************");
+            Console.WriteLine($"Hello, {userName} {lastName}!");
+            Console.WriteLine($"You are currectly {userAge} years old. In 5 years, you will be {futureAge}.");
+            Console.WriteLine($"The original price of the item is ${productPrice}");
+            Console.WriteLine($"As a student, your discount price is ${StudentDiscount}");
+            Console.WriteLine($"As a senior citizen, your discounted price would be ${SeniorCitizen}.");
+            Console.WriteLine($"**************************************************************");
         }
     }
 }
